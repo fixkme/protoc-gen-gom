b908998e6b27d0eac811f46005ed9bab4115f70b
xinli1dragonplus <xin.li1@dragonplus.com>
2025-04-14 18:50:20 +0800

    rpc

--- example/pbout/go/game/game.rpc.go
+++ example/pbout/go/game/game.rpc.go
@@ -2,15 +2,16 @@ package game
 
 import (
 	context "context"
+	grpc "google.golang.org/grpc"
 	proto "google.golang.org/protobuf/proto"
 )
 
 type GameServer interface {
+	GameAddActivity(context.Context, *CGameAddActivity) (*SGameAddActivity, error)
 	GameUpdateActivity(context.Context, *CGameUpdateActivity) (*SGameUpdateActivity, error)
 	GameUpdateActivityStatus(context.Context, *CGameUpdateActivityStatus) (*SGameUpdateActivityStatus, error)
 	GetActivityList(context.Context, *CGetActivityList) (*SGetActivityList, error)
 	LoginGame(context.Context, *CLoginGame) (*SLoginGame, error)
-	GameAddActivity(context.Context, *CGameAddActivity) (*SGameAddActivity, error)
 }
 
 func _Game_GetActivityList_Handler(srv any, ctx context.Context, in proto.Message) (any, error) {
@@ -36,4 +37,31 @@ func _Game_GameUpdateActivity_Handler(srv any, ctx context.Context, in proto.Mes
 func _Game_GameUpdateActivityStatus_Handler(srv any, ctx context.Context, in proto.Message) (any, error) {
 	req := in.(*CGameUpdateActivityStatus)
 	return srv.(GameServer).GameUpdateActivityStatus(ctx, req)
+}
+
+var Game_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "Game",
+	HandlerType: (*GameServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetActivityList",
+			Handler:    _Game_GetActivityList_Handler,
+		},
+		{
+			MethodName: "LoginGame",
+			Handler:    _Game_LoginGame_Handler,
+		},
+		{
+			MethodName: "GameAddActivity",
+			Handler:    _Game_GameAddActivity_Handler,
+		},
+		{
+			MethodName: "GameUpdateActivity",
+			Handler:    _Game_GameUpdateActivity_Handler,
+		},
+		{
+			MethodName: "GameUpdateActivityStatus",
+			Handler:    _Game_GameUpdateActivityStatus_Handler,
+		},
+	},
 }
